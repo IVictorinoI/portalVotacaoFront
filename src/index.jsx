fc0f582f7507d6851a7fcf41060e53b27c8f37f0
IVictorinoI <victormatheusmendes@hotmail.com>
2020-05-02 16:36:17 -0300

    'publicacao'

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ import Routes from './main/routes'
 import reducers from './main/reducers'
 
 window.Params = {
-    URL_API: 'http://localhost:3000/api/',
+    URL_API: 'https://gladius-portalvotacao-back.herokuapp.com/api/',
     codigoAssembleiaAtiva: 5
 }
 

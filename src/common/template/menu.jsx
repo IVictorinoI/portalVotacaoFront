53d98ec0ab0da015c0df9c857545cf5dfaefe504
IVictorinoI <victormatheusmendes@hotmail.com>
2020-06-07 20:55:30 -0300

    Ajustes na listagem de credores para mostrar online e quem votou e quem confirmou

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react'
+
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import MenuItem from './menuItem'
 import MenuTree from './menuTree'
+import If from '../operator/if'
 
-export default props => (
-    <ul className='sidebar-menu'>
-        <MenuItem path='/' label='Dashboard' icon='dashboard' />
-        {/*<MenuItem path='login' label='Login' icon='user' />*/}
-        <MenuTree label='Consultas' icon='edit'> 
-            {/*<MenuItem path='assembleias' label='Assembléias' icon='usd' />*/}
-            {/*<MenuItem path='classes' label='Classes' icon='usd' />*/}
-            <MenuItem path='credores' label='Credores' icon='drivers-license-o' />
-            <MenuItem path='votos' label='Votos' icon='server' />
-            <MenuItem path='online' label='Usuários online' icon='power-off' />
-            <MenuItem path='logacesso' label='Logs de acesso' icon='info' />
-        </MenuTree>
-        <MenuItem path='confirmarPresenca' label='Confirmar presença' icon='check-square' />
-        <MenuItem path='votar' label='Votar' icon='legal' />
-        <MenuItem path='votacaoTempoReal' label='Votação em tempo real' icon='heartbeat' />
-        <MenuItem path='chat' label='Chat' icon='comments' />
-    </ul>
-)
+class Menu extends Component {
+    render() {
+        const { tipo } = this.props.user
+        const isTipoUsuarioGladius = tipo === 1;
+        return (
+            <ul className='sidebar-menu'>
+                <MenuItem path='/' label='Dashboard' icon='dashboard' />
+                {/*<MenuItem path='login' label='Login' icon='user' />*/}
+                <MenuTree label='Consultas' icon='edit'> 
+                    {/*<MenuItem path='assembleias' label='Assembléias' icon='usd' />*/}
+                    {/*<MenuItem path='classes' label='Classes' icon='usd' />*/}
+                    <MenuItem path='credores' label='Credores' icon='drivers-license-o' />
+                    <MenuItem path='votos' label='Votos' icon='server' />
+                    <MenuItem path='online' label='Usuários online' icon='power-off' />
+                    <If test={isTipoUsuarioGladius}>
+                        <MenuItem path='logacesso' label='Logs de acesso' icon='info' />
+                    </If>
+                </MenuTree>
+                <MenuItem path='confirmarPresenca' label='Confirmar presença' icon='check-square' />
+                <MenuItem path='votar' label='Votar' icon='legal' />
+                <MenuItem path='votacaoTempoReal' label='Votação em tempo real' icon='heartbeat' />
+                <MenuItem path='chat' label='Chat' icon='comments' />
+            </ul>
+        )
+    }
+}
+
+const mapStateToProps = state => ({ user: state.auth.user })
+export default connect(mapStateToProps)(Menu)
--- src/credor/credorList.jsx
+++ src/credor/credorList.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import './credor.css'
 
 export default props => {
 
+
+    const classNameRow = (credor) => {
+        if(credor.votou)
+            return 'info'
+        if(credor.confirmou)
+            return 'success'
+
+        return 'active';
+    }
+
     const renderRows = () => {
         const list = props.list || []
+        
         return list.map(todo => (
-            <tr key={todo._id}>
+            <tr key={todo._id} className={classNameRow(todo)}>
+                <td>{todo.online ? <div className='bola'></div> : <div></div>}</td>
                 <td>{todo.nome}</td>
                 <td>{todo.descricaoClasse}</td>
                 <td>{todo.cpfCnpj}</td>
                 <td style={{ textAlign: 'right' }}>{todo.valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
                 <td>{todo.nomeProcurador}</td>
+                <td>{todo.votou ? "Sim" : "Não"}</td>
+                <td>{todo.confirmou ? "Sim" : "Não"}</td>
             </tr>
         ))
     }
@@ -19,11 +34,14 @@ export default props => {
         <table className='table table-hover'>
             <thead>
                 <tr>
+                    <th></th>
                     <th>Nome</th>
                     <th>Classe</th>
                     <th>Cpf/Cnpj</th>
                     <th>Crédito</th>
                     <th>Procurador</th>
+                    <th>Votou</th>
+                    <th>Confirmou</th>
                 </tr>
             </thead>
             <tbody>

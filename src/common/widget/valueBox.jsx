17f42ff99cf25a2dec89463f79cb2909a8c5ad51
Victor Matheus Mendes <victormatheusmendes@hotmail.com>
2021-05-20 21:07:22 -0300

    xxxyyzzz

--- src/common/widget/valueBox.jsx
+++ src/common/widget/valueBox.jsx
@@ -6,8 +6,8 @@ export default props => (
     <Grid cols={props.cols}> 
         <div className={`small-box bg-${props.color}`}> 
             <div className='inner'> 
-                <h4 data-tip={props.infoValorEsperado}>{props.valorEsperado}</h4>
-                <h4 data-tip={props.infoValorConfirmado}>{props.valorConfirmado}</h4>
+                <h4 data-tip={props.infoValorEsperado}>{props.valorEsperado} <font size="1">{props.infoValorEsperado}</font></h4>
+                <h4 data-tip={props.infoValorConfirmado}>{props.valorConfirmado} <font size="1">{props.infoValorConfirmado}</font></h4>
                 <h4>{props.percentual}</h4>
                 <p>{props.text}</p>
             </div> 

ee29b44b8ff64a9b307b7fc906b504f09b69b1bb
Victor Matheus Mendes <victormatheusmendes@hotmail.com>
2021-07-26 22:45:16 -0300

    value box ordem

--- src/common/widget/valueBox.jsx
+++ src/common/widget/valueBox.jsx
@@ -6,10 +6,10 @@ export default props => (
     <Grid cols={props.cols}> 
         <div className={`small-box bg-${props.color}`}> 
             <div className='inner'> 
+                <p>{props.text}</p>
                 <h4 data-tip={props.infoValorEsperado}>{props.valorEsperado} <font size="1">{props.infoValorEsperado}</font></h4>
                 <h4 data-tip={props.infoValorConfirmado}>{props.valorConfirmado} <font size="1">{props.infoValorConfirmado}</font></h4>
-                <h4>{props.percentual}</h4>
-                <p>{props.text}</p>
+                <h4>{props.percentual}</h4>                
             </div> 
             <div className='icon'> 
                 <i className={`fa fa-${props.icon}`}></i>

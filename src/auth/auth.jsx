6353daf11f00b8f54784b132368596a6b7006fa0
Victor Matheus Mendes <victormatheusmendes@hotmail.com>
2021-08-22 20:35:42 -0300

    Textos do login e nova logo

--- src/auth/auth.jsx
+++ src/auth/auth.jsx
@@ -26,37 +26,60 @@ class Auth extends Component {
         const loginMode = true;
         const { handleSubmit } = this.props
         return (
-            <div className="login-box">
-                <div className="login-logo"> Votação</div>
-                <div className="login-box-body">
-                    <p className="login-box-msg">Bem vindo!</p>
-                    <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
-                        <Field component={Input} type="input" name="nome"
-                            placeholder="Nome" icon='user' hide={loginMode} />
-                        <Field component={Input} type="input" name="login"
-                            placeholder="Login" icon='user' />
-                        <Field component={Input} type="email" name="email"
-                            placeholder="E-mail" icon='envelope' hide={loginMode} />
-                        <Field component={Input} type="password" name="senha"
-                            placeholder="Senha" icon='lock' />
-                        <Field component={Input} type="password" name="confirm_senha"
-                            placeholder="Confirmar Senha" icon='lock' hide={loginMode} />
-                        <Row>
-                            <Grid cols="4">
-                                <button type="submit"
-                                    className="btn btn-primary btn-block btn-flat">
-                                    {loginMode ? 'Entrar' : 'Registrar'}
-                                </button>
-                            </Grid>
-                        </Row>
-                    </form>
-                    <br />
-                    {/*<a onClick={() => this.changeMode()}>
-                        {loginMode ? 'Novo usuário? Registrar aqui!' :
-                            'Já é cadastrado? Entrar aqui!'}
-                    </a>*/}
+            <div>
+                <div className="login-box">
+                    <div className="login-logo"><img src="./logo_login.png" alt="Votação gladius" width="256" height="115"></img></div>
+                    <p className="login-box-msg login-title-font">Bem-Vindo ao Sistema de Assembleia Online da Gladius Consultoria</p>
+                    <div className="login-box-body">
+                        <p className="login-box-msg">Digite o login e senha enviados previamente por e-mail</p>
+                        <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
+                            <Field component={Input} type="input" name="nome"
+                                placeholder="Nome" icon='user' hide={loginMode} />
+                            <Field component={Input} type="input" name="login"
+                                placeholder="Login" icon='user' />
+                            <Field component={Input} type="email" name="email"
+                                placeholder="E-mail" icon='envelope' hide={loginMode} />
+                            <Field component={Input} type="password" name="senha"
+                                placeholder="Senha" icon='lock' />
+                            <Field component={Input} type="password" name="confirm_senha"
+                                placeholder="Confirmar Senha" icon='lock' hide={loginMode} />
+                            <Row>
+                                <Grid cols="12">
+                                    <button type="submit"
+                                        className="btn btn-primary btn-block btn-flat">
+                                        {loginMode ? 'Entrar' : 'Registrar'}
+                                    </button>
+                                </Grid>
+                            </Row>
+                        </form>
+                        <br />
+                        {/*<a onClick={() => this.changeMode()}>
+                            {loginMode ? 'Novo usuário? Registrar aqui!' :
+                                'Já é cadastrado? Entrar aqui!'}
+                        </a>*/}
+                    </div>                
+                    <Messages />
                 </div>
-                <Messages />
+                <div>
+
+                    <p className="login-box-msg login-title-font">Caso não consiga acessar o sistema, entre em contato com a Administração Judicial através das vias abaixo: </p>
+                    <center>
+                        <table className="table-contacts-style">
+                        <tr>
+                            <td width="100"><b>Telefones:</b></td>
+                            <td>(48) 3433-8525</td>
+                        </tr>
+                        <tr>
+                            <td width="100"></td>
+                            <td>(48) 9102-5411 – WhatsApp</td>
+                        </tr>
+                        <tr>
+                            <td width="100"><b>E-mail:</b></td>
+                            <td>atendimento@gladiusconsultoria.com.br</td>
+                        </tr>
+                        </table>
+                    </center>                    
+                </div>
             </div>
         )
     }
